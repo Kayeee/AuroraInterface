ee8f8c1fcdd48a585c632ecad148275eda539831
Kevin <kevin@Kevins-MBP.lan>
2016-08-10 13:57:27 -0700

    Added data simulation, Y flags, updater method merged into tasks file

--- inverterInterface.py
+++ inverterInterface.py
@@ -9,37 +9,37 @@ class Inverter:
     serialPort = "/dev/ttyUSB0"
 
     def getCumulatedEnergies(self):
-        result = subprocess.Popen(["aurora", "-a", self.address, "-e", "-J", self.serialPort], stdout=subprocess.PIPE)
+        result = subprocess.Popen(["aurora", "-a", self.address, "-e", "-J", self.serialPort, "-Y", "10"], stdout=subprocess.PIPE)
         out, err = result.communicate()
         return out
 
     def getDSP(self, stringNum = "0"):
-        result = subprocess.Popen(["aurora", "-a", self.address, "-d", stringNum, "-J", self.serialPort], stdout=subprocess.PIPE)
+        result = subprocess.Popen(["aurora", "-a", self.address, "-d", stringNum, "-J", self.serialPort, "-Y", "10"], stdout=subprocess.PIPE)
         out, err = result.communicate()
         return out
 
     def getPartNum(self):
-        result = subprocess.Popen(["aurora", "-a", self.address, "-p", "-J", self.serialPort], stdout=subprocess.PIPE)
+        result = subprocess.Popen(["aurora", "-a", self.address, "-p", "-J", self.serialPort, "-Y", "10"], stdout=subprocess.PIPE)
         out, err = result.communicate()
         return out
 
     def getSerialNum(self):
-        result = subprocess.Popen(["aurora", "-a", self.address, "-n", "-J", self.serialPort], stdout=subprocess.PIPE)
+        result = subprocess.Popen(["aurora", "-a", self.address, "-n", "-J", self.serialPort, "-Y", "10"], stdout=subprocess.PIPE)
         out, err = result.communicate()
         return out
 
     def getFirmWare(self):
-        result = subprocess.Popen(["aurora", "-a", self.address, "-f", "-J", self.serialPort], stdout=subprocess.PIPE)
+        result = subprocess.Popen(["aurora", "-a", self.address, "-f", "-J", self.serialPort, "-Y", "10"], stdout=subprocess.PIPE)
         out, err = result.communicate()
         return out
 
     def getManufacturingDate(self):
-        result = subprocess.Popen(["aurora", "-a", self.address, "-m", "-J", self.serialPort], stdout=subprocess.PIPE)
+        result = subprocess.Popen(["aurora", "-a", self.address, "-m", "-J", self.serialPort, "-Y", "10"], stdout=subprocess.PIPE)
         out, err = result.communicate()
         return out
 
     def getVersion(self):
-        result = subprocess.Popen(["aurora", "-a", self.address, "-v", "-J", self.serialPort], stdout=subprocess.PIPE)
+        result = subprocess.Popen(["aurora", "-a", self.address, "-v", "-J", self.serialPort, "-Y", "10"], stdout=subprocess.PIPE)
         out, err = result.communicate()
         return out
 
@@ -49,12 +49,12 @@ class Inverter:
         return out
 
     def getData(self, stringNum = "0"):
-        result = subprocess.Popen(["aurora", "-a", self.address, "-e", "-d", stringNum, "-J", self.serialPort], stdout=subprocess.PIPE)
+        result = subprocess.Popen(["aurora", "-a", self.address, "-e", "-d", stringNum, "-J", self.serialPort, "-Y", "10"], stdout=subprocess.PIPE)
         out, err = result.communicate()
         return out
 
     def getInfo(self):
-        result = subprocess.Popen(["aurora", "-a", self.address,  "-p", "-n", "-f", "-m", "-v", "-J", self.serialPort], stdout=subprocess.PIPE)
+        result = subprocess.Popen(["aurora", "-a", self.address,  "-p", "-n", "-f", "-m", "-v", "-J", self.serialPort, "-Y", "10"], stdout=subprocess.PIPE)
         out, err = result.communicate()
         return out
 
--- tasks.py
+++ tasks.py
@@ -2,10 +2,9 @@ from inverterInterface import Inverter, ConnectionException
 from celery import Celery
 
 import subprocess
+import random
 
 app = Celery('tasks', backend='amqp', broker='amqp://Kevin:ASUi3dea@54.224.107.102/pi_env')
-
-Inverter
 
 @app.task
 def add(x, y):
@@ -41,9 +40,44 @@ def updateAuroraC():
     subprocess.call(["git", "pull", "origin", "master"], cwd="Documents/i3dea/aurora-1.9.0") #need ssh key for this
     subprocess.call(["sudo", "make", "install"], cwd="Documents/i3dea/aurora-1.9.0")
 
-@app.task
-def updateTasksAndInterface():
-    subprocess.call(["git", "pull", "origin", "master"])
-
 def JSONify(data):
     return "{" + data + "}"
+
+@app.task
+def createData():
+    input_voltage = "\"inputvoltage\": {:.5f},".format(random.uniform(220.0, 260.0))
+    input_current = "\"inputcurrent\": {:.5f},".format(random.uniform(4.0, 7.0))
+    input_power = "\"inputpower\": {:.5f},".format(random.uniform(1300.0, 1500.0))
+    grid_voltage = "\"gridvoltage\": {:.5f},".format(random.uniform(220.0, 260.0))
+    grid_current = "\"gridcurrent\": {:.5f},".format(random.uniform(4.0, 7.0))
+    grid_power = "\"gridpower\": {:.5f},".format(random.uniform(1300.0, 1500.0))
+    frequency = "\"frequency\": {:.5f},".format(random.uniform(50.0, 70.0))
+    conversion_efficiency = "\"conversionefficiency\": {:.1f},".format(random.uniform(80.0, 99.0))
+    inverter_temperature = "\"invertertemperature\": {:.5f},".format(random.uniform(80.0, 99.0))
+    daily_energy = "\"dailyenergy\": {:.5f},".format(random.uniform(0.1, 50.0))
+    weekly_energy = "\"weeklyenergy\": {:.5f},".format(random.uniform(0.1, 120.0))
+    monthly_energy = "\"monthlyenergy\": {:.5f},".format(random.uniform(0.1, 600.0))
+    yearly_energy = "\"yearlyenergy\": {:.5f},".format(random.uniform(0.1, 4000.0))
+    total_energy = "\"totalenergy\": {:.5f}".format(random.uniform(15000.1, 16000.0))
+
+    return """
+    {{
+        \"inverter\": "0",
+        {0}
+        {1}
+        {2}
+        {3}
+        {4}
+        {5}
+        {6}
+        {7}
+        {8}
+        \"cumulatedenergy\": {{
+            {9}
+            {10}
+            {11}
+            {12}
+            {13}
+        }}
+    }}
+    """.format(input_voltage, input_current, input_power, grid_voltage, grid_current, grid_power, frequency, conversion_efficiency, inverter_temperature, daily_energy, weekly_energy, monthly_energy, yearly_energy, total_energy)
