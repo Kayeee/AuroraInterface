f750cec887e4fc5409d70e6d47b4a33eddd9b7eb
Prafull <praf1249@gmail.com>
2016-09-07 13:13:22 -0700

    new repo of tasks and updater

---
+++ inverterInterface.py
@@ -0,0 +1,71 @@
+import subprocess
+
+#subprocess.call(["ls", "-l"])
+class ConnectionException(Exception):
+    pass
+
+class Inverter:
+    address = "2"
+    serialPort = "/dev/ttyUSB0"
+
+    def getCumulatedEnergies(self):
+        result = subprocess.Popen(["aurora", "-a", self.address, "-e", "-J", self.serialPort, "-Y", "10"], stdout=subprocess.PIPE)
+        out, err = result.communicate()
+        return out
+
+    def getDSP(self, stringNum = "0"):
+        result = subprocess.Popen(["aurora", "-a", self.address, "-d", stringNum, "-J", self.serialPort, "-Y", "10"], stdout=subprocess.PIPE)
+        out, err = result.communicate()
+        return out
+
+    def getPartNum(self):
+        result = subprocess.Popen(["aurora", "-a", self.address, "-p", "-J", self.serialPort, "-Y", "10"], stdout=subprocess.PIPE)
+        out, err = result.communicate()
+        return out
+
+    def getSerialNum(self):
+        result = subprocess.Popen(["aurora", "-a", self.address, "-n", "-J", self.serialPort, "-Y", "10"], stdout=subprocess.PIPE)
+        out, err = result.communicate()
+        return out
+
+    def getFirmWare(self):
+        result = subprocess.Popen(["aurora", "-a", self.address, "-f", "-J", self.serialPort, "-Y", "10"], stdout=subprocess.PIPE)
+        out, err = result.communicate()
+        return out
+
+    def getManufacturingDate(self):
+        result = subprocess.Popen(["aurora", "-a", self.address, "-m", "-J", self.serialPort, "-Y", "10"], stdout=subprocess.PIPE)
+        out, err = result.communicate()
+        return out
+
+    def getVersion(self):
+        result = subprocess.Popen(["aurora", "-a", self.address, "-v", "-J", self.serialPort, "-Y", "10"], stdout=subprocess.PIPE)
+        out, err = result.communicate()
+        return out
+
+    def getAll(self, stringNum = "0"):
+        result = subprocess.Popen(["aurora", "-a", self.address, "-e", "-d", stringNum, "-p", "-n", "-f", "-v", "-J", self.serialPort, "-Y", "10"], stdout=subprocess.PIPE)
+        out, err = result.communicate()
+        return out
+
+    def getData(self, stringNum = "0"):
+        result = subprocess.Popen(["aurora", "-a", self.address, "-e", "-d", stringNum, "-J", self.serialPort, "-Y", "10"], stdout=subprocess.PIPE)
+        out, err = result.communicate()
+        return out
+
+    def getInfo(self):
+        result = subprocess.Popen(["aurora", "-a", self.address,  "-p", "-n", "-f", "-m", "-v", "-J", self.serialPort, "-Y", "10"], stdout=subprocess.PIPE)
+        out, err = result.communicate()
+        return out
+
+    def checkException(self, result):
+        if "ERROR:" in result:
+            raise ConnectionException("Error: Cannot connect to inverter.")
+
+    def runErrorCheck(self):
+        raise ConnectionException("Error: Testing")
+
+    def getAlle(self, stringNum = "0"):
+        result = subprocess.Popen(["aurorae", "-a", self.address, "-e", "-d", stringNum, "-p", "-n", "-f", "-v", "-J", self.serialPort, "-Y", "10"], stdout=subprocess.PIPE)
+        out, err = result.communicate()
+        return out
