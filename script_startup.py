6d210482173c60c85e8b8c0d71180cd59939c5e1
Prafull Gaikwad <praf1249@gmail.com>
2016-09-14 18:17:41 +0000

    update commands to be run

--- script_startup.py
+++ script_startup.py
@@ -1,7 +1,7 @@
 import subprocess
 
 def updateTasksAndInterface():
-    subprocess.call(["git", "pull", "origin", "master"], cwd="/home/pi/ASUi3dea")
-    subprocess.call(["celery", "multi", "start", "updater", "-A", "updater"], cwd="/home/pi/ASUi3dea")
+    subprocess.call(["git", "pull", "origin", "master"], cwd="/home/pi/Desktop/RaspberryPi_ProjectPrototype/bootup/auroraInterface")
+    subprocess.call(["celery", "multi", "start", "updater", "-A", "updater", "-l", "debug"], cwd="/home/pi/Desktop/RaspberryPi_ProjectPrototype/bootup/auroraInterface")
 
 updateTasksAndInterface()
